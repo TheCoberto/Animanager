ee5759fa29c73ac32a841e6e0a207359c7d93bb5
TheCoberto <coby.kelly.ck@gmail.com>
2024-02-22 01:14:47 -0500

    fix repo

---
+++ Animanager.UI/Controller.cs
@@ -0,0 +1,80 @@
+﻿using Animanager.Models;
+using System;
+
+namespace Animanager.UI
+{
+    public class Controller
+    {
+        Repository repo;
+        public Controller()
+        {
+            repo = new Repository();
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                DisplayMenu();
+
+                int.TryParse(Console.ReadLine(), out var result);
+
+                switch (result)
+                {
+                    case 1:
+                        Create();
+                        break;
+                    case 2:
+                        ReadAll();
+                        break;
+                    case 3:
+                        Update();
+                        break;
+                    case 4:
+                        Delete();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Animanager. Please select from the list of options.");
+            Console.WriteLine("1. Create");
+            Console.WriteLine("2. Read All");
+        }
+
+        public void Create()
+        {
+            Animal animal = new Animal();
+            Console.WriteLine("Enter a name for the new animal.");
+            animal.Name = Console.ReadLine();
+            repo.Create(animal);
+        }
+        public void ReadAll()
+        {
+            Console.Clear();
+            var animals = repo.ReadAll();
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.Name);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Enter any key to continue.");
+            Console.ReadLine();
+        }
+        public void Update()
+        {
+            Repository.Update();
+        }
+        public void Delete()
+        {
+            Repository.Delete();
+        }
+    }
+}
---
+++ Animanager.UI/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Animanager.Models;
+
+namespace Animanager.UI
+{
+    public class Repository
+    {
+        public List<Animal> animals;
+
+        public Repository() 
+        {
+            animals = new List<Animal>();
+        }
+
+        public void Create(Animal animal)
+        {
+            animals.Add(animal);
+        }
+
+        public List<Animal> ReadAll()
+        {
+            return animals;
+        }
+
+        internal static void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
